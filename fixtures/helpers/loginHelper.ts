4d04aa870d5461253ac4afabfa24a10ae5469938
h.esmaeili <h.esmaeili@avaparsi.com>
2025-10-25 01:51:17 +0330

    add login and register page and its flow in the helper

---
+++ fixtures/helpers/loginHelper.ts
@@ -0,0 +1,57 @@
+import { Page, expect } from '@playwright/test';
+import { LoginPage } from '../../pages/loginPage';
+import { loginData } from '../../fixtures/data/loginData';
+
+export async function loginWithCredentials(
+  page: Page,
+  username: string,
+  password: string
+) {
+  const loginPage = new LoginPage(page);
+  await loginPage.navigateToLogin();
+  await expect(loginPage.loginBtn).toBeVisible();
+  await expect(loginPage.newUserBtn).toBeEnabled();
+  await loginPage.login(username, password);
+
+}
+
+
+export async function registration(
+    page:Page,
+    firstname: string,
+    lastname: string,
+    username: string,
+    password: string
+) {
+  const loginPage = new LoginPage(page);
+  await loginPage.navigateToRegister();
+  await expect (loginPage.registerContent).toBeVisible();
+  await loginPage.newUser(firstname, lastname, username, password)
+}
+
+
+export async function loginAsValidUser(page: Page) {
+  const loginPage = new LoginPage(page);
+  await loginWithCredentials(page, loginData.valid.userName, loginData.valid.password);
+  await expect(page).toHaveURL("profile");
+  await expect(loginPage.logoutBtn).toBeVisible();
+  await expect(loginPage.userNameValue).toBe(loginData.valid.userName);
+}
+
+
+export async function loginAsInvalidUser(page: Page) {
+  const loginPage = new LoginPage(page);
+  await loginWithCredentials(page, loginData.invalid.userName, loginData.invalid.password);
+  await loginPage.expectLoginFailed();
+}
+
+
+export async function registerAsNewUser(page:Page) {
+    const loginPage = new LoginPage(page);
+    await registration(page, loginData.register.firstName, loginData.register.lastName, loginData.register.userName, loginData.register.password);
+    page.once('dialog', async (dialog) => {
+    expect(dialog.message()).toBe('User Register Successfully');
+    await dialog.accept();
+  });
+}
+
---
+++ pages/loginPage.ts
@@ -0,0 +1,75 @@
+import { Locator, Page, expect } from '@playwright/test';
+import { LookupAddress } from 'dns';
+
+export class LoginPage {
+  readonly page: Page;
+  readonly loginContent: Locator;
+  readonly usernameInput: Locator;
+  readonly passwordInput: Locator;
+  readonly loginBtn: Locator;
+  readonly newUserBtn: Locator;
+  readonly registerContent: Locator;
+  readonly firstName: Locator;
+  readonly lastName: Locator;
+  readonly registerationUserName: Locator;
+  readonly registrationPassword: Locator;
+  readonly captcha: Locator;
+  readonly captchaCheckMark: Locator;
+  readonly registerBtn: Locator;
+  readonly errorMsg: Locator;
+  readonly userExistMsg: Locator;
+  readonly logoutBtn: Locator;
+  readonly userNameValue: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.loginContent = page.locator('//h1[text()="Login"]');
+    this.usernameInput = page.locator('#userName');
+    this.passwordInput = page.locator('#password');
+    this.loginBtn = page.locator('#login');
+    this.newUserBtn = page.locator('#newUser');
+    this.registerContent = page.locator('//h1[text()="Register"]')
+    this.firstName = page.locator('#firstname');
+    this.lastName = page.locator('#lastname');
+    this.registerationUserName = page.locator('#userName');
+    this.registrationPassword = page.locator('#password');
+    this.captcha = page.locator('//label[@id="recaptcha-anchor-label"]');
+    this.captchaCheckMark = page.locator('//div[@class="recaptcha-checkbox-checkmark"]');
+    this.registerBtn = page.locator('#register');
+    this.errorMsg = page.locator('#name');
+    this.userExistMsg = page.locator('//p[@id="name" and text()="User exists!"]');
+    this.logoutBtn = page.locator('(//button[@id="submit"])[1]');
+    this.userNameValue = page.locator('//label[@id="userName-value"]')
+  }
+
+  async navigateToLogin() {
+    await this.page.goto('/login');
+  }
+  
+  async navigateToRegister() {
+    await this.page.goto('/register');
+  }
+  
+  async login(username: string, password: string) {
+    await expect(this.loginContent).toBeVisible();
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginBtn.click();
+  }
+
+  async newUser(firstName: string, lastName: string, registerationUserName: string, registrationPassword: string) {
+    await this.firstName.fill(firstName);
+    await this.lastName.fill(lastName);
+    await this.registerationUserName.fill(registerationUserName);
+    await this.registrationPassword.fill(registrationPassword);
+    await this.captcha.click();
+    await expect(this.captchaCheckMark).toBeVisible();
+    await this.registerBtn.click();
+  }
+
+  async expectLoginFailed() {
+    await expect(this.errorMsg).toBeVisible();
+    await expect(this.errorMsg).toHaveText('Invalid username or password!');
+  }
+}
+
